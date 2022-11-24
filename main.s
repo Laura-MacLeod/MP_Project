3e5b93fdcd156634ca5a40bdb03be7e842d18058
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-11-24 12:00:52 +0000

    buzzer operation

--- main.s
+++ main.s
@@ -2,23 +2,26 @@
 
 psect	code, abs
 	
+	
 main:
-	org	0x0
-	goto	start
+    goto buzzer
+buzzer:
+    movlw   0x0		;SETTING PERIOD BY WRITING TO PR2
+    movwf   PR2
+    movlw   0x0		;SETTING DUTY CYCLE BY WRITING TO CCPR4L
+    movwf   CCPR4L
+    movlw   0x0F	;SETTING DUTY CYCLE
+    movwf   CCP4CON 
+    movlw   0x00
+    movf    TRISG	;CLEAR TRIS BIT, MAKE CCP4 AN OUTPUT
+    movlw   0xff
+    movwf   TMR2
+    movlw   0xff
+    movwf   T2CON
+    bra	    buzzer
+    
 
-	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+    
+    end main
 
-	end	main
+
