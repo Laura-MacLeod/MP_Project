36ff342e2b041300e5c2b1a773fe584a25d08a3d
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-11-24 13:46:48 +0000

    Merge origin/ADC_test_measurement_V5.4 into ADC_test_measurement_V5.4
    Conflicts:
    	ACD.s

--- main.s
+++ main.s
@@ -1,25 +1,17 @@
 	#include <xc.inc>
 
+extrn	buzzer
+	
 psect	code, abs
 	
 	;THIS IS A GIT TEST
+
+
 	
 main:
-    goto buzzer
-buzzer:
-    movlw   0x0		;SETTING PERIOD BY WRITING TO PR2
-    movwf   PR2
-    movlw   0x0		;SETTING DUTY CYCLE BY WRITING TO CCPR4L
-    movwf   CCPR4L
-    movlw   0x0F	;SETTING DUTY CYCLE
-    movwf   CCP4CON 
-    movlw   0x00
-    movf    TRISG	;CLEAR TRIS BIT, MAKE CCP4 AN OUTPUT
-    movlw   0xff
-    movwf   TMR2
-    movlw   0xff
-    movwf   T2CON
-    bra	    buzzer
+    goto buzzer_loop
+buzzer_loop:
+    call buzzer
     
 
     
