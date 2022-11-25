e10e09adfe26114eeb1196ecc992ef7c0bd122c2
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-11-25 09:33:15 +0000

    Merge origin/ADC_test_measurement_V5.4 into ADC_test_measurement_V5.4
    Conflicts:
    	ACD.s

--- buzzer_module.s
+++ buzzer_module.s
@@ -9,23 +9,26 @@ psect	code, abs
 	
 
 buzzer:
-    movlw   0x0		;SETTING PERIOD BY WRITING TO PR2
+    movlw   0xF9		;SETTING PERIOD BY WRITING TO PR2
     movwf   PR2
     
-    ; PWM duty cycle = 10 bit value (CCPR1L:CCP1CON<5:4>) ? TOSC ? (TMR2 Prescale Value)
+    ; PWM duty cycle = 10 bit value (CCPR1L:CCP1CON<5:4>) * TOSC * (TMR2 Prescale Value)
     
-    movlw   0x0		;SETTING DUTY CYCLE BY WRITING TO CCPR4L
+    movlw   0x3E    	;SETTING DUTY CYCLE BY WRITING TO CCPR4L
     movwf   CCPR4L	; CCPR4L is 8 MSBs of duty cycle
     
-    movlw   0x0F	;SETTING DUTY CYCLE
+    movlw   0x1C	;SETTING DUTY CYCLE
     movwf   CCP4CON	; CCP4CON register bits 7-6 are 0, bits 5-4 are LSBs of duty cycle
 			; bits 3-0 are 11xx (try 1100, 0x0C) for PWM mode
-    movlw   0x0C
+    movlw   0x00
     movf    TRISG	;CLEAR TRIS BIT, MAKE CCP4 AN OUTPUT
-    movlw   0xff
+    
+    movlw   0x00
     movwf   TMR2
-    movlw   0xff
+    
+    movlw   0x04
     movwf   T2CON
+    
     bra	    buzzer
     return
 
