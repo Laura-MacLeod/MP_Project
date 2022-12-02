42cee990554c8ec42edb47ee21fbc8789e0e07e8
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-12-02 11:19:14 +0000

    02/12/2022
    Fully functional, varying duty cycle

--- main.s
+++ main.s
@@ -2,9 +2,14 @@
 
 ;EXTRN	PORT_INIT, CLOCK_INIT, TIMER_INIT, PWM_INIT, SIGNAL
 EXTRN	duty_cycle_upper, duty_cycle_lower
-EXTRN PWM_INIT, TIMER_INIT, CLOCK_INIT, PORT_INIT	
+EXTRN PWM_INIT, TIMER_INIT, CLOCK_INIT, PORT_INIT, SIGNAL	
 
-	;THIS IS A GIT TEST
+psect	udata_acs
+	
+delay_count:	ds 1
+
+
+	
 psect	code,abs
 
 	
@@ -24,17 +29,207 @@ INIT:
     
     
 MAIN:
-    
+
     ; Set duty cycle
 
     MOVLW   0xFF		; (random)
     MOVWF   duty_cycle_upper
     
-    MOVLW   0x00111100 		;must be 0b00xx1100 
-    MOVWF   duty_cycle_lower
+    ;MOVLW   0x00111100 		;must be 0b00xx1100 
+    ;MOVWF   duty_cycle_lower
 
+    
+    
+    CALL	    DELAY
+    
+    MOVLW   0xEF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xDF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    
+    CALL	    DELAY
+    
+    MOVLW   0xCF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xBF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xAF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x9F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x8F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x7F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x6F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x5F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x4F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x3F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x2F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x1F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x2F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x3F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x4F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x5F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL    DELAY
+    
+    MOVLW   0x6F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x7F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x8F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0x9F		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xAF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xBF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xCF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xDF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xEF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    CALL	    DELAY
+    
+    MOVLW   0xFF		
+    MOVWF   duty_cycle_upper
+    CALL    SIGNAL
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
    ; CALL    SIGNAL
     BRA	    MAIN
+
+    
+DELAY:	
+	MOVLW   0XFF
+	MOVWF   delay_count
 	
+	decfsz	delay_count, A	; decrement until zero
+	bra	DELAY
+	return
+    
+    
 
 END	RST   
