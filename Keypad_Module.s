638cf8d2705ebde85765d985ce90ac289ee7e9f8
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-12-08 12:06:01 +0000

    08/12/2022
    Functional keypad for key C, duty cycle values still dodgy even with fixed values imported from Python

---
+++ Keypad_Module.s
@@ -0,0 +1,939 @@
+	#include <xc.inc>
+	
+
+global	Fsharp
+global  keypad_setup, keypad_read_row, keypad_read_column, combine, interpret
+extrn	SIGNAL, duty_cycle_upper
+psect	udata_acs
+delay_count:	ds 1
+row:	    ds    1	    ; reserve 1 byte for variable UART_counter
+column:	    ds    1
+value:	    ds	  1
+rowvals:    ds	  1
+columnvals: ds	  1
+delay_counter:	    ds	  1
+compare_value:	    ds	  1
+	
+ psect	pwm_code, class=CODE
+    
+
+ 
+ 
+ keypad_setup:
+	movlb	15	    ;go to BSR 15
+	bsf	REPU	    ;set pullup resistors high
+	movlb	0	    ;return t0 access bank
+	
+	clrf	LATE
+	clrf    TRISE
+	return
+	
+	
+	
+keypad_read_row:
+    
+	movlw   0xf0
+	movwf   TRISE
+	
+	call	delay_seq  
+
+	movf	PORTE, W
+	movff	PORTE, row
+	
+	movlw	0xf0	    
+	cpfseq	row	;Compare keypad inputs to 00001111, if the same, skip
+	movff   row, rowvals	;Stores value at row
+	return			;Doesn't store value
+
+keypad_read_column:
+
+	movlw   0x0f
+	movwf   TRISE
+	
+	call	delay_seq
+	
+	movf	PORTE, W
+	movff	PORTE, column
+	
+	movlw	0x0f	    
+	cpfseq	column		    ;Compare keypad inputs to 11110000, if the same, skip
+	movff   column, columnvals  ;Stores value at column
+	return			    ;Doesn't store value
+    
+	
+ combine:
+	movf	column, W
+	iorwf	row, W		    ;Combines columns with rows and move to W
+	nop
+	movwf	value
+	return
+
+ compare_combine:
+	movf	column, W
+	iorwf	row, W		    ;Combines columns with rows and move to W
+	nop
+	movwf	compare_value
+	return
+	
+	
+	
+	
+	
+interpret:
+	movlw	0xff		    ;NO BUTTON PRESSED
+	cpfseq	value		    
+	bra	next1
+	retlw	0x00		    ;RETURN NULL
+
+next1:
+	movlw	0x77	    ;ROW 4 COLUMN 4 PRESSED
+	cpfseq	value		    
+	bra	next2
+	call	Fsharp
+	retlw	'c'		    ;RETURN 'C'
+next2:
+	movlw	0xB7	    ;ROW 3 COLUMN 4 PRESSED
+	cpfseq	value		    
+	bra	next3
+	retlw	'd'		    ;RETURN 'D'
+
+next3:
+	movlw	0x7B	    ;ROW 4 COLUMN 3 PRESSED
+	cpfseq	value		    
+	bra	next4
+	retlw	'b'		    ;RETURN 'B'
+
+next4:
+	movlw	0xD7	    ;ROW 2 COLUMN 4 PRESSED
+	cpfseq	value		    
+	bra	next5
+	retlw	'e'		    ;RETURN 'E'
+
+next5:
+	movlw	0x7D	    ;ROW 4 COLUMN 2 PRESSED
+	cpfseq	value		    
+	bra	next6
+	retlw	'0'		    ;RETURN '0'
+
+next6:
+	movlw	0xBB	    ;ROW 3 COLUMN 3 PRESSED
+	cpfseq	value		    
+	bra	next7
+	retlw	'9'		    ;RETURN '9'
+
+next7:
+	movlw	0xBD	    ;ROW 3 COLUMN 2 PRESSED
+	cpfseq	value		    
+	bra	next8
+	retlw	'8'		    ;RETURN '8'
+
+next8:
+	movlw	0xdb	    ;ROW 2 COLUMN 3 PRESSED
+	cpfseq	value		    
+	bra	next9
+	nop
+	retlw	'6'		    ;RETURN '6'
+	
+	return
+
+next9:
+	movlw	0x7E	    ;ROW 4 COLUMN 1 PRESSED
+	cpfseq	value		    
+	bra	next10
+	retlw	'a'		    ;RETURN 'A'
+
+next10:
+	movlw	0xE7	    ;ROW 1 COLUMN 4 PRESSED
+	cpfseq	value		    
+	bra	next11
+	retlw	'f'		    ;RETURN 'F'
+
+next11:
+	movlw	0xDD	    ;ROW 2 COLUMN 2 PRESSED
+	cpfseq	value		    
+	bra	next12
+	retlw	'5'		    ;RETURN '5'
+
+next12:
+	movlw	0xBE	    ;ROW 3 COLUMN 1 PRESSED
+	cpfseq	value		    
+	bra	next13
+	retlw	'7'		    ;RETURN '7'
+
+next13:
+	movlw	0xEB	    ;ROW 1 COLUMN 3 PRESSED
+	cpfseq	value		    
+	bra	next14
+	retlw	'3'		    ;RETURN '3'
+
+next14:
+	movlw	0xED	    ;ROW 1 COLUMN 2 PRESSED
+	cpfseq	value		    
+	bra	next15
+	retlw	'2'		    ;RETURN '2'
+
+next15:
+	movlw	0xDE	    ;ROW 2 COLUMN 1 PRESSED
+	cpfseq	value		    
+	bra	next16
+	retlw	'4'		    ;RETURN '4'
+
+next16:
+	movlw	0xEE	    ;ROW 1 COLUMN 1 PRESSED
+	cpfseq	value		    
+	bra	errorfunc
+	retlw	'1'		    ;RETURN '1'
+
+errorfunc:
+	retlw	0xff		    ;ERROR
+	return
+    
+    
+delay_seq:
+	movlw	0x90		    ; 144 operations = 9us
+	movwf	delay_counter
+	bra	delay
+delay:
+	decfsz	delay_counter, A	; decrement until zero
+	bra	delay
+	return
+
+ 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+ 
+Fsharp:
+    MOVLW   111011 
+
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   1101110 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   1110010 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   1000100 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   1110 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   10 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   101011 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   1100011 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   1110110 
+    MOVWF   duty_cycle_upper 
+    CALL    SIGNAL 
+    CALL     DELAY 
+    MOVLW   1010100 
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   11011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   0 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   11011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1010100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110111 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1100010 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   101010 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   10 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1000100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110010 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1101101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   111011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1001 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   110011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1101001 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1001100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   10100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   0 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   100011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1011100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110111 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1011011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   100010 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   0 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   10101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1001101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1101000 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   110010 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1010 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   111100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1101110 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110001 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1000011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   10 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   101100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1100011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110110 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1010011 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   11010 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   0 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   11100 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1010101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1110111 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1100001 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   101001 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   10 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1111 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+
+
+    MOVLW   1000101 
+
+    MOVWF   duty_cycle_upper 
+
+
+
+    CALL    SIGNAL 
+
+    CALL     DELAY 
+
+    
+    
+    call	keypad_read_row
+    call	DELAY
+    call	keypad_read_column
+    
+    call	compare_combine
+    
+    movf	value, W	   
+    cpfseq	compare_value
+    return
+    bra		Fsharp
+
+    
+    	
+	
+    
+DELAY:
+	movlw	0x90		    ; 144 operations = 9us
+	movwf	delay_counter
+	bra	DELAY_SEQ
+DELAY_SEQ:
+	decfsz	delay_counter, A	; decrement until zero
+	bra	DELAY_SEQ
+	return
+
+
+    bra	Fsharp
+    
+END
