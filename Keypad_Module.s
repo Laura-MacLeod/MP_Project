e054579f1c99f1e5c65f4aeb9273061d47499a64
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-12-12 15:48:11 +0000

    12/12/2022
    Midway-through converting to interrupt version, may be somewhat incomprehensible and definitely will not run.

--- Keypad_Module.s
+++ Keypad_Module.s
@@ -2,7 +2,7 @@
 	
 
 global	Fsharp
-global  keypad_setup, keypad_read_row, keypad_read_column, combine, interpret
+global  KEYPAD_SETUP, keypad_read_row, keypad_read_column, combine, interpret
 extrn	SIGNAL, duty_cycle_upper
 psect	udata_acs
 delay_count:	ds 1
@@ -19,7 +19,7 @@ compare_value:	    ds	  1
 
  
  
- keypad_setup:
+ KEYPAD_SETUP:
 	movlb	15	    ;go to BSR 15
 	bsf	REPU	    ;set pullup resistors high
 	movlb	0	    ;return t0 access bank
@@ -45,156 +45,46 @@ keypad_read_row:
 	movff   row, rowvals	;Stores value at row
 	return			;Doesn't store value
 
-keypad_read_column:
+keypad_read_column:		    ; 1 op
 
-	movlw   0x0f
-	movwf   TRISE
+	movlw   0x0f		    ; 1 op
+	movwf   TRISE		    ; 1 op
 	
-	call	delay_seq
+	call	delay_seq	    ; 144 ops
 	
-	movf	PORTE, W
-	movff	PORTE, column
+	movf	PORTE, W	    ; 1 op
+	movff	PORTE, column	    ; 1 op
 	
-	movlw	0x0f	    
-	cpfseq	column		    ;Compare keypad inputs to 11110000, if the same, skip
-	movff   column, columnvals  ;Stores value at column
-	return			    ;Doesn't store value
+	movlw	0x0f		    ; 1 op
+	cpfseq	column		    ; 1 op			;Compare keypad inputs to 11110000, if the same, skip
+	movff   column, columnvals  ; 1 op			;Stores value at column
+	return			    ; 1 op			;Doesn't store value
     
 	
  combine:
 	movf	column, W
 	iorwf	row, W		    ;Combines columns with rows and move to W
-	nop
 	movwf	value
 	return
 
- compare_combine:
-	movf	column, W
-	iorwf	row, W		    ;Combines columns with rows and move to W
-	nop
-	movwf	compare_value
-	return
+ compare_combine:		    ; 1 op
+	movf	column, W	    ; 1 op
+	iorwf	row, W		    ; 1 op           ;Combines columns with rows and move to W
+	movwf	compare_value	    ; 1 op
+	return			    ; 1 op
 	
 	
 	
 	
 	
-interpret:
-	movlw	0xff		    ;NO BUTTON PRESSED
-	cpfseq	value		    
-	bra	next1
-	retlw	0x00		    ;RETURN NULL
 
-next1:
-	movlw	0x77	    ;ROW 4 COLUMN 4 PRESSED
-	cpfseq	value		    
-	bra	next2
-	call	Fsharp
-	retlw	'c'		    ;RETURN 'C'
-next2:
-	movlw	0xB7	    ;ROW 3 COLUMN 4 PRESSED
-	cpfseq	value		    
-	bra	next3
-	retlw	'd'		    ;RETURN 'D'
-
-next3:
-	movlw	0x7B	    ;ROW 4 COLUMN 3 PRESSED
-	cpfseq	value		    
-	bra	next4
-	retlw	'b'		    ;RETURN 'B'
-
-next4:
-	movlw	0xD7	    ;ROW 2 COLUMN 4 PRESSED
-	cpfseq	value		    
-	bra	next5
-	retlw	'e'		    ;RETURN 'E'
-
-next5:
-	movlw	0x7D	    ;ROW 4 COLUMN 2 PRESSED
-	cpfseq	value		    
-	bra	next6
-	retlw	'0'		    ;RETURN '0'
-
-next6:
-	movlw	0xBB	    ;ROW 3 COLUMN 3 PRESSED
-	cpfseq	value		    
-	bra	next7
-	retlw	'9'		    ;RETURN '9'
-
-next7:
-	movlw	0xBD	    ;ROW 3 COLUMN 2 PRESSED
-	cpfseq	value		    
-	bra	next8
-	retlw	'8'		    ;RETURN '8'
-
-next8:
-	movlw	0xdb	    ;ROW 2 COLUMN 3 PRESSED
-	cpfseq	value		    
-	bra	next9
-	nop
-	retlw	'6'		    ;RETURN '6'
-	
-	return
-
-next9:
-	movlw	0x7E	    ;ROW 4 COLUMN 1 PRESSED
-	cpfseq	value		    
-	bra	next10
-	retlw	'a'		    ;RETURN 'A'
-
-next10:
-	movlw	0xE7	    ;ROW 1 COLUMN 4 PRESSED
-	cpfseq	value		    
-	bra	next11
-	retlw	'f'		    ;RETURN 'F'
-
-next11:
-	movlw	0xDD	    ;ROW 2 COLUMN 2 PRESSED
-	cpfseq	value		    
-	bra	next12
-	retlw	'5'		    ;RETURN '5'
-
-next12:
-	movlw	0xBE	    ;ROW 3 COLUMN 1 PRESSED
-	cpfseq	value		    
-	bra	next13
-	retlw	'7'		    ;RETURN '7'
-
-next13:
-	movlw	0xEB	    ;ROW 1 COLUMN 3 PRESSED
-	cpfseq	value		    
-	bra	next14
-	retlw	'3'		    ;RETURN '3'
-
-next14:
-	movlw	0xED	    ;ROW 1 COLUMN 2 PRESSED
-	cpfseq	value		    
-	bra	next15
-	retlw	'2'		    ;RETURN '2'
-
-next15:
-	movlw	0xDE	    ;ROW 2 COLUMN 1 PRESSED
-	cpfseq	value		    
-	bra	next16
-	retlw	'4'		    ;RETURN '4'
-
-next16:
-	movlw	0xEE	    ;ROW 1 COLUMN 1 PRESSED
-	cpfseq	value		    
-	bra	errorfunc
-	retlw	'1'		    ;RETURN '1'
-
-errorfunc:
-	retlw	0xff		    ;ERROR
-	return
     
     
 delay_seq:
-	movlw	0x90		    ; 144 operations = 9us
-	movwf	delay_counter
-	bra	delay
+	movlw	0x46		    ; 70 operations
+	movwf	delay_counter, A
 delay:
-	decfsz	delay_counter, A	; decrement until zero
+	decfsz	delay_counter, A	; 70 * 2 + 4 operations = 144 operations = 9us (including call)
 	bra	delay
 	return
 
@@ -212,19 +102,19 @@ delay:
 	
 	
  
-Fsharp:
+Fsharp:			    ; 740 Hz
     
-    MOVLW   0x63     	    ; 0b11111100
-    MOVWF   PR2
+    MOVLW   0x63     	    ; 1 op
+    MOVWF   PR2		    ; 1 op
 
     
-    MOVLW    0x32 
+MOVLW    0x32		    ; 1 op
 
-MOVWF    duty_cycle_upper 
+MOVWF    duty_cycle_upper   ; 1 op
 
-CALL     SIGNAL 
+CALL     SIGNAL		    ; 3 op
 
-CALL     DELAY 
+CALL     DELAY		    ; ?
 
  
 
@@ -353,7 +243,7 @@ MOVLW    0x4e
 MOVWF    duty_cycle_upper 
 
 CALL     SIGNAL 
-
+    
 CALL     DELAY 
 
  
@@ -534,32 +424,54 @@ MOVWF    duty_cycle_upper
 
 CALL     SIGNAL 
 
-CALL     DELAY 
-
+CALL     DELAY
     
-    call	keypad_read_row
-    call	DELAY
-    call	keypad_read_column
+;CALL	DELAY_CHECK   
     
-    call	compare_combine
     
-    movf	value, W	   
-    cpfseq	compare_value
+;    
+;    call	keypad_read_row		    ; 153 ops		    ;
+;    call	keypad_read_column	    ; 153 ops
+;    call	compare_combine		    ; 5 op
+;    
+;    movf	value, W		    ; 1 op
+;    cpfseq	compare_value		    ; 1 op
     return
-    bra		Fsharp
+    
+    
+    
+goto		Fsharp			    ; 1 op
+
+
+    	
 
     
-    	
-	
+DELAY_CHECK:
+    
+    call	keypad_read_row		    ; 153 ops		    ;
+    call	keypad_read_column	    ; 153 ops
+    call	compare_combine		    ; 5 op
+    
+    movf	value, W		    ; 1 op
+    cpfseq	compare_value		    ; 1 op
+    bra		interpret
+    return
+    
+    
+    
+    
+    
+    
  DELAY:
-	movlw	0x20		       ; 144 operations = 9us
+
+	movlw	0x10		       ; 144 operations = 9us
 	movwf	delay_counter, A
 	bra	DELAY_SEQ1
 DELAY_SEQ1:
 	decfsz	delay_counter, A	; decrement until zero
 	bra	DELAY_SEQ1
 	
-	movlw	0x01		    ; 144 operations = 9us
+	movlw	0x02		    ; 144 operations = 9us
 	movwf	delay_counter, A
 	bra	DELAY_SEQ2
 DELAY_SEQ2:
@@ -577,14 +489,6 @@ DELAY_SEQ2:
 	
 	return
  
-;DELAY:
-;	movlw	0xff		    ; 144 operations = 9us
-;	movwf	delay_counter
-;	bra	DELAY_SEQ
-;DELAY_SEQ:
-;	decfsz	delay_counter, A	; decrement until zero
-;	bra	DELAY_SEQ
-;	return
 
 
     bra	Fsharp
