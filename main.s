b3ea4ab7e87f172005fde958b900e905bc0197d9
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2022-12-13 16:26:12 +0000

    13/12/2022
    RESET TO PRE-INTERRUPT

--- main.s
+++ main.s
@@ -2,81 +2,78 @@
 
 ;EXTRN	PORT_INIT, CLOCK_INIT, TIMER_INIT, PWM_INIT, SIGNAL
 EXTRN	duty_cycle_upper, duty_cycle_lower
-EXTRN	PWM_INIT, TIMER_INIT, CLOCK_INIT, PORT_INIT, SIGNAL, KEYPAD_SETUP, keypad_read_row, keypad_read_column, combine, interpret	
-
-extrn	INT_HIGH, INT_ON
-	
-GLOBAL	Countdown
-	
+EXTRN	PWM_INIT, TIMER_INIT, CLOCK_INIT, PORT_INIT, SIGNAL	
+extrn	keypad_setup, keypad_read_row, keypad_read_column, combine, interpret
+extrn	Fsharp
 psect	udata_acs
 	
 delay_counter1:	    ds 1
-delay_counter2:	    ds 1   
-length:		    ds 1
-store:		    ds 1
-Countdown:	    ds	1
+delay_counter2:	    ds 1
+counter:    ds 1    ; reserve one byte for a counter variable
+length:	    ds 1
+store:	    ds 1
 
+
+; GIT TEST
+	
+psect	code,abs
 
 	
-	
-psect	code, abs
-
 RST:
     
-    ORG	    0x0000
+    ORG	    0x00
     GOTO    INIT
     
-INTERRUPT:	
     
-    org	    0x0008	; high vector, no low vector
-    goto    INT_HIGH
+    
+    
     
     
     org 0x100
-    
-    
 INIT:
     CALL    PORT_INIT
     CALL    CLOCK_INIT
     CALL    TIMER_INIT
     CALL    PWM_INIT
-    CALL    KEYPAD_SETUP
+    CALL    keypad_setup
     
-    bsf	GIE		; Enable all interrupts
-    bsf	TMR0IE		; Enable timer2 interrupt
+    GOTO    start
     
-    MOVLW    0x65 
-    MOVWF    duty_cycle_upper 
-    CALL     SIGNAL
-    
- 
-;    CALL	INT_ON
-    
-    movlw   0X10
-    movwf   Countdown		; Initialise countdown depending on number of samples/periods per sinusoid
-    
-    GOTO    MAIN
-    
-    MAIN:
-    
-;	CALL	INT_ON
-    
-
-
-
-;	
-;    MOVLW   0X63	    ; 0b11111111
-;    MOVWF   PR2
-;    
-
-
+start:
+    nop
 	call	keypad_read_row
+;	call	DELAY1
+	nop
 	call	keypad_read_column
 	call	combine
+
+	nop
 	call	interpret
 	
-;	
-	bra	MAIN
+	bra	start
 	
+
+
+DELAY1:
+	movlw	0x90		    ; 144 operations = 9us
+	movwf	delay_counter1
+	bra	DELAY1_SEQ
+DELAY1_SEQ:
+	decfsz	delay_counter1, A	; decrement until zero
+	bra	DELAY1_SEQ
+	return
+	
+DELAY2:	
+	MOVLW   0X11
+	MOVWF   delay_counter2, A
+	
+DELAY2_SEQ:
+	decfsz	delay_counter2, A	; decrement until zero
+	bra	DELAY2_SEQ
+	
+	
+	return
+    
+    
 
 END	RST   
