29e25b0bed4befc8de371d3913115c242cdba639
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2023-01-14 17:20:19 +0000

    Tidied

--- main.s
+++ main.s
@@ -1,21 +1,8 @@
 	#include <xc.inc>
 
-;EXTRN	PORT_INIT, CLOCK_INIT, TIMER_INIT, PWM_INIT, SIGNAL
-EXTRN	duty_cycle_upper, duty_cycle_lower
-EXTRN	PWM_INIT, TIMER_INIT, CLOCK_INIT, PORT_INIT, SIGNAL	
+EXTRN	PWM_INIT, TIMER_INIT, CLOCK_INIT, PORT_INIT, SIGNAL, LCD_Setup	
 extrn	keypad_setup, keypad_read_row, keypad_read_column, combine, interpret
-extrn	letter1, letter2, LCD_Send_Byte_D, LCD_Setup, LCD_Send_Byte_I
-psect	udata_acs
-	
-delay_counter1:	    ds 1
-delay_counter2:	    ds 1
-counter:    ds 1    ; reserve one byte for a counter variable
-length:	    ds 1
-store:	    ds 1
 
-
-; GIT TEST
-	
 psect	code,abs
 
 	
@@ -23,13 +10,10 @@ RST:
     
     ORG	    0x00
     GOTO    INIT
-    
-    
-    
-    
-    
-    
+   
     org 0x100
+    
+    
 INIT:
     CALL    PORT_INIT
     CALL    CLOCK_INIT
@@ -38,39 +22,19 @@ INIT:
     CALL    keypad_setup
     CALL    LCD_Setup
 	
-    
     GOTO    start
+    
     
 start:
 
-	call	keypad_read_row		    ; 153 ops
-	call	keypad_read_column	    ; 153 ops
-	call	combine
+	call	keypad_read_row		
+	call	keypad_read_column	    
+	call	combine		    ; Combine values read from keypad into characteristic byte for specific key
 
-	call	interpret		    ; 72 ops from interpret, 4 ops in note so 76 ops
+	call	interpret	    ; Interpret input from keypad, including varying correct duty cycles for one period of output sinusoid  
 
-	bra	start			    ; 1 op
-	
-; total ops to loop = 384 ops = 2.4e-5s = 24us
+	bra	start			  
 
-DELAY1:
-	movlw	0x90		    ; 144 operations = 9us
-	movwf	delay_counter1, A
-	bra	DELAY1_SEQ
-DELAY1_SEQ:
-	decfsz	delay_counter1, A	; decrement until zero
-	bra	DELAY1_SEQ
-	return
-	
-DELAY2:	
-	MOVLW   0X11
-	MOVWF   delay_counter2, A
-	
-DELAY2_SEQ:
-	decfsz	delay_counter2, A	; decrement until zero
-	bra	DELAY2_SEQ
-	
-	
 	return
     
     
