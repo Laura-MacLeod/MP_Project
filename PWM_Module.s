6407c31985bacd438f151f464134e9f024e7dbad
Laura-MacLeod <97027376+Laura-MacLeod@users.noreply.github.com>
2023-01-14 17:22:25 +0000

    Tidied

--- PWM_Module.s
+++ PWM_Module.s
@@ -8,16 +8,16 @@ psect	udata_acs
 	
 duty_cycle_upper:	ds  1
 duty_cycle_lower:	ds  1
-clock_speed_bits:	ds  1
     
     
  psect	pwm_code, class=CODE
     
+ 
 PORT_INIT:
 
     BCF	    TRISC, 2	    ; Set P1A/RC2 pin to output
-    
     RETURN
+    
     
     
 CLOCK_INIT:
@@ -28,10 +28,7 @@ CLOCK_INIT:
     BSF	    OSCCON, 6	    ; 1  ;Oscillator Speed is 8MHz ; Fosc = 8MHz
     BSF	    OSCCON, 5	    ; 1	
     BCF	    OSCCON, 4	    ; 0
-    
-    MOVLW   00000110		    ; Store three bits OSCCON<6:4> was set to like 00000<6:4>
-    MOVWF   clock_speed_bits	    ; This is for calculating the clock frequency and thus 
-    
+
     RETURN
     
     
@@ -40,7 +37,7 @@ TIMER_INIT:
     
     CLRF    TMR2	    ; Clearing timer2/counter
     
-    MOVLW   01110110	    ; 0b11111111
+    MOVLW   0x76	    ; 0b01110110
     MOVWF   PR2		    ; Set period of signal,  PR2 = (Fosc/(4*PWMfreq*prescale)) - 1; Equation 19-1
     
     MOVLW   0xFC	    ; 0b11111100
@@ -50,13 +47,13 @@ TIMER_INIT:
     
     
     
-PWM_INIT:
+PWM_INIT:		    ; For enhanced CCP module 1
     
     MOVLW   0x3C	    ; 0b00111100 
     MOVWF   CCP1CON	    ; <7:6> output mode set 00, <5:4> duty cycle 2 LSBs, <3:0> set 11xx for PWM mode, ideally 1100
     
-    MOVLW   0x64	    ; 0b00000011
-    MOVWF   CCPR1L	    ; Set duty cycle,   (CCPR1L:CCP1CON<5:4>) = PWM Duty Cycle /  (TOSC • (TMR2 Prescale Value)); Equation 19-2
+    MOVLW   0x64	    ; 0b01100100
+    MOVWF   CCPR1L	    ; Set duty cycle,   (CCPR1L:CCP1CON<5:4>) = PWM Duty Cycle /  (TOSC â¢ (TMR2 Prescale Value)); Equation 19-2
     
     MOVLW   0x01	    ; 0b00000001
     MOVWF   PSTR1CON	    ; To steer pulse to RC2/P1A pin only, set to 00000001
@@ -73,13 +70,9 @@ SIGNAL:
     ; Duty cycle value should be prewritten to 'duty_cycle'
     
     MOVFF   duty_cycle_upper, CCPR1L	    ;set new duty cycle (8 MSBs)
-    ;MOVFF   duty_cycle_lower, CCP1CON	    ;set new duty cycle (2 LSBs), might not be necessary
+    ;MOVFF   duty_cycle_lower, CCP1CON	    ;set new duty cycle (2 LSBs), only necessary for extra precision
     
     RETURN
-    
-    
-CALC_PERIOD:
-    ; PR2 = (Fosc/(4*PWMfreq*prescale)) - 1
-    
+
 	
 END
